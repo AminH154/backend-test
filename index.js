c99c42c9c2697222bd70c4ac37fcc41182db428b
amin <aminbouallegui0@gmail.com>
2025-04-15 01:06:38 +0200

    commit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-
 const Product = require('./models/product.model');
+
+//midleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+//routes
+app.use('/api/products',productRoutes);
+
+
+
+
+
+
+//create a product
 
 app.post('/api/products',async  (req,res) =>{
     try {
@@ -15,6 +30,9 @@ app.post('/api/products',async  (req,res) =>{
         res.status(500).json({message : error.message});
     }
     });
+
+
+//get a product by id
 app.get('/api/products/:id', async (req,res) =>{
   try{
     const { id } = req.params;
@@ -25,6 +43,7 @@ app.get('/api/products/:id', async (req,res) =>{
     res.status(500).json({message : error.message});
   }
 })
+//get all products
 
 app.get('/api/products',async (req,res)=> {
   try{
@@ -34,6 +53,8 @@ app.get('/api/products',async (req,res)=> {
     res.status(500).json({message : error.message});
   }
 })
+
+//update product
 app.put('/api/products/:id',async (req,res)=>{
   const { id }= req.params ; 
   const product =await Product.findByIdAndUpdate(id,req.body);
@@ -48,11 +69,6 @@ catch(error){
   res.status(500).json({message : error.message});
 };
 });
-
-
-
-
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
@@ -60,6 +76,28 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.send("Hello World gays!");
 });
+
+
+//connect to MongoDB
 mongoose.connect('mongodb+srv://aminbouallegui0:qMS27GuBCnfvLrAH@node.xqn764z.mongodb.net/node-API?retryWrites=true&w=majority&appName=node')
   .then(() => console.log('Connected!'))
   .catch(err => console.log(err));
+
+
+
+//suprimer un produit
+app.delete('/api/products/:id',async (req,res)=>{
+  try{
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product){
+      return res.status(404).json({message : "product not found"});
+    }
+    res.status(200).json({message : "product deleted"});
+
+  }
+  catch(error){
+    res.status(500).jason({message : error.message});
+
+  }
+})
