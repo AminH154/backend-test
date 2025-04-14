a06fd7128108e5ae67fadcb0fb48cfd6621829c4
amin <aminbouallegui0@gmail.com>
2025-04-14 20:36:06 +0200

    commit

--- index.js
+++ index.js
@@ -15,8 +15,39 @@ app.post('/api/products',async  (req,res) =>{
         res.status(500).json({message : error.message});
     }
     });
+app.get('/api/products/:id', async (req,res) =>{
+  try{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+     res.status(200).json(product);
+  }
+  catch(error){
+    res.status(500).json({message : error.message});
+  }
+})
 
-
+app.get('/api/products',async (req,res)=> {
+  try{
+      const products = await Product.find({})
+      res.status(200).json(products);
+  }catch(error){
+    res.status(500).json({message : error.message});
+  }
+})
+app.put('/api/products/:id',async (req,res)=>{
+  const { id }= req.params ; 
+  const product =await Product.findByIdAndUpdate(id,req.body);
+  try{
+  if (!product){
+    return res.status(404).json({message : "product not found"});
+  }
+  const updatProduct = await Product.findById(id);
+  res.status(200).json(updatProduct);
+  }
+catch(error){
+  res.status(500).json({message : error.message});
+};
+});
 
 
 
